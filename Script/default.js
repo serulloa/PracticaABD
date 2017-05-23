876ac83ac8251478f69cbe0339f08822acb63cca
Sergio Ulloa <serulloa@ucm.es>
2017-05-23 15:32:30 +0200

    JS arreglado, falta registro

--- Script/default.js
+++ Script/default.js
@@ -1,10 +1,21 @@
  function closeModal() {
   var modal = document.getElementById('indexModal');
-  var cancel = document.getElementById('cancelbtn')
+  var cancel = document.getElementById('cancelbtn');
 
   window.onclick = function(event) {
     if (event.target == modal || event.target == cancel) {
       modal.style.display = "none";
+    }
+  }
+}
+
+function showModal() {
+  var modal = document.getElementById('indexModal');
+  var registrate = document.getElementById('indexButtonSignup');
+
+  window.onclick = function(event) {
+    if (event.target == registrate) {
+      modal.style.display = "block";
     }
   }
 }
